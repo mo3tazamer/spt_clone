8e681d397710e94a8fcfb49360344cd597569a73
Mo3taz <moatazamer7319@gmail.com>
2024-05-01 23:53:42 +0300

    Initial commit

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,10 @@
+const String _iconsPath = 'assets/icons';
+const String _imagesPath = 'assets/images/';
+
+
+class IconAssets{
+  static const String splashIcon = '$_iconsPath/splash_icon.svg' ;
+
+
+
+}
