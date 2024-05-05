ff396aae2d4face1cd054f13a52a08c5bb020305
Mo3taz <moatazamer7319@gmail.com>
2024-05-05 04:56:59 +0300

    Initial3 commit

--- lib/features/auth/presentation/screens/otp_screen.dart
+++ lib/features/auth/presentation/screens/otp_screen.dart
@@ -1,6 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:pinput/pinput.dart';
 import 'package:spt_clone/core/shared/widgets/scaffold_red_corner.dart';
